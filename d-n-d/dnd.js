e6420547eac6ef21dcac00f2dd25d89292950e04
Sergii_Mekhonoshyn <sergii_mekhonoshyn@epam.com>
2014-11-01 00:10:22 +0200

    minor non-functional changes
    updated call method, removed extra method call into onDoDrag, added
    test page

--- d-n-d/dnd.js
+++ d-n-d/dnd.js
@@ -2,42 +2,40 @@
  * created by mekhonoshyn on 10/31/14.
  */
 
-// tTarget - transformation target - translatable element (f.e. panel)
-// dSTarget - drag-start target - draggable element (f.e. header of target panel)
-// dETarget - drag-end target - container within which is possible to perform dragging
-// initialX - initial shift by x-Axis (relatively to rendered place)
-// initialY - initial shift by y-Axis (relatively to rendered place)
+/**
+ * tTarget  - (required) -  transformation target - translatable element (f.e. panel)
+ * dSTarget -               drag-start target - draggable element (f.e. header of target panel)
+ * dETarget -               drag-end target - container within which is possible to perform dragging
+ * initialX -               initial shift by x-Axis (relatively to rendered place)
+ * initialY -               initial shift by y-Axis (relatively to rendered place)
+ * callback -               if provided will be executed on each drag-end event with passing final X and Y
+ */
 
-//add class 'no-dragging' (by default) to any child element to forbid child element to be draggable
+/**
+ * add class 'no-dragging' (by default) to any child element to forbid child element to be draggable
+ */
 
 /*jslint browser:true */
-
-_DnDFactory = (function () {
+_DnDFactory = (function _DnDFactoryInitializer() {
     var mouseMoveEvent = 'mousemove',
         mouseUpEvent = 'mouseup',
         mouseDownEvent = 'mousedown',
         noDraggingClass = 'no-dragging',
-        transformationMatrixTemplate = ['translate(', 0, 'px, ', 0, 'px)'];
+        transformationMatrixTemplate = ['translate(', 0, 'px, ', 0, 'px)'],
+        eventListenerToken = 'EventListener',
+        eventTargetPrototype = EventTarget.prototype;
 
-    function _DnDMagic(config) {
+    eventTargetPrototype._on = eventTargetPrototype['add' + eventListenerToken];
+    eventTargetPrototype._un = eventTargetPrototype['remove' + eventListenerToken];
+
+    function _DnDMagic(tTarget, dETarget, dSTarget, initialX, initialY, onDragEndCallback) {
         var x,
             y,
-            savedX,
-            savedY,
-            savedPageX,
-            savedPageY,
-            transformationMatrix = transformationMatrixTemplate.slice(),
-            dETarget = config.dETarget || document,
-            tTarget = config.tTarget,
-            onDragEndCallback = config.callback;
-
-        function _applyTransformation(newX, newY) {
-            transformationMatrix[1] = x = newX;
-
-            transformationMatrix[3] = y = newY;
-
-            tTarget.style.transform = transformationMatrix.join('');
-        }
+            savedX = 0,
+            savedY = 0,
+            savedPageX = 0,
+            savedPageY = 0,
+            transformationMatrix = transformationMatrixTemplate.slice();
 
         function _onStartDrag(event) {
             if (event.button || event.target.classList.contains(noDraggingClass)) {
@@ -50,13 +48,17 @@ _DnDFactory = (function () {
             savedPageX = event.pageX;
             savedPageY = event.pageY;
 
-            dETarget.addEventListener(mouseMoveEvent, _onDoDrag);
+            dETarget._on(mouseMoveEvent, _onDoDrag);
 
-            dETarget.addEventListener(mouseUpEvent, _onEndDrag);
+            dETarget._on(mouseUpEvent, _onEndDrag);
         }
 
         function _onDoDrag(event) {
-            _applyTransformation(savedX + event.pageX - savedPageX, savedY + event.pageY - savedPageY);
+            transformationMatrix[1] = x = savedX + event.pageX - savedPageX;
+
+            transformationMatrix[3] = y = savedY + event.pageY - savedPageY;
+
+            tTarget.style.transform = transformationMatrix.join('');
         }
 
         function _onEndDrag() {
@@ -64,17 +66,27 @@ _DnDFactory = (function () {
                 onDragEndCallback(x, y);
             }
 
-            dETarget.removeEventListener(mouseMoveEvent, _onDoDrag);
+            dETarget._un(mouseMoveEvent, _onDoDrag);
 
-            dETarget.removeEventListener(mouseUpEvent, _onEndDrag);
+            dETarget._un(mouseUpEvent, _onEndDrag);
         }
 
-        config.dSTarget.addEventListener(mouseDownEvent, _onStartDrag);
+        dSTarget._on(mouseDownEvent, _onStartDrag);
 
-        _applyTransformation(config.initialX || 0, config.initialY || 0);
+        _onDoDrag({
+            pageX: initialX,
+            pageY: initialY
+        });
     }
 
     return function _DnDFactory(config) {
-        return new _DnDMagic(config);
+        return new _DnDMagic(
+            config.tTarget,
+            config.dETarget || document,
+            config.dSTarget || config.tTarget,
+            config.initialX || 0,
+            config.initialY || 0,
+            config.callback
+        );
     }
 })();
